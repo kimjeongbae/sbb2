945bdf53ec9ab5a8107db80f3a127202a6672fd3
kimjeongbae <gprhf651@naver.com>
2024-01-09 14:09:01 +0900

    psuh

---
+++ src/test/java/com/example/sbb2/Sbb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
